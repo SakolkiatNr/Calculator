dc0f721c9f91009cbea8154515371dd00d959a99
Sakolkiat <sakolkiat.n@gmail.com>
2025-05-26 09:56:18 +0700

    Add updateNumberDisplay function
    When number button is being click
    This function will automatically change the number display

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function add(a, b) {
-    console.log(`${a} + ${b} = ${a+b}`)
     return a + b;
 }
 
@@ -19,4 +18,28 @@ function operate(a, b, operator) {
     return operator(a, b)
 }
 
-operate(1,2,add);
+const numberDisplay = document.querySelector('.number-display');
+const numberButtons = document.querySelectorAll('.number');
+
+function updateNumberDisplay() {
+    numberButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            // If default number already 0
+            if (button.value == '0') {
+                if (numberDisplay.textContent == '0') return;
+            }
+            // Number only contain 1 dot
+            if (button.value == '.') {
+                if (numberDisplay.textContent.includes('.')) return;
+            }
+            // remove 0 if value is more than 0
+            if (numberDisplay.textContent.charAt(0) == '0' &&
+                numberDisplay.textContent.charAt(1) != '.' &&
+                button.value !== '0' && button.value !== '.') {
+                    numberDisplay.textContent = "";
+                }
+            numberDisplay.textContent += button.value; 
+        })
+    });
+}
+updateNumberDisplay();
